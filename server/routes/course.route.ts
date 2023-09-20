b43f8515a8eb5f869a66deed2450492da0221917
BidhanCR <bidhanroy300805@gmail.com>
2023-09-20 12:08:58 +0600

    add reivew and review reply api created

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,19 +1,46 @@
 import express from "express";
-import { addAnswer, addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import {
+  addAnswer,
+  addQuestion,
+  addReplyToReview,
+  addReview,
+  editCourse,
+  getAllCourses,
+  getCourseByUser,
+  getSingleCourse,
+  uploadCourse,
+} from "../controllers/course.controller";
 import { authorizedRoles, isAuthenticated } from "../middleware/auth";
 
 const courseRouter = express.Router();
 
-courseRouter.post("/create-course", isAuthenticated, authorizedRoles("admin"),  uploadCourse);
+courseRouter.post(
+  "/create-course",
+  isAuthenticated,
+  authorizedRoles("admin"),
+  uploadCourse
+);
 
-courseRouter.put("/edit-course/:id", isAuthenticated, authorizedRoles("admin"),  editCourse);
-courseRouter.get("/get-course/:id",  getSingleCourse);
-courseRouter.get("/get-courses",  getAllCourses);
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizedRoles("admin"),
+  editCourse
+);
+courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourses);
 courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
 courseRouter.put("/add-question", isAuthenticated, addQuestion);
 
 courseRouter.put("/add-answer", isAuthenticated, addAnswer);
 
+courseRouter.put("/add-review/:id", isAuthenticated, addReview);
 
+courseRouter.put(
+  "/add-reply",
+  isAuthenticated,
+  authorizedRoles("admin"),
+  addReplyToReview
+);
 
 export default courseRouter;
